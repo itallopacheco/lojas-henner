db44f33af2fce40c9c65fea54795a2eb2389b943
Itallo Pacheco <oicapivara@academico.ufs.brm>
2022-10-25 01:46:04 -0300

    fix: mensagens de erro em portugues na autenticação de usuario

--- cadastro/views.py
+++ cadastro/views.py
@@ -1,10 +1,20 @@
+from argparse import Action
 from multiprocessing.connection import Client
 from django.http import JsonResponse
 from cadastro.serializer import ClientesSerializer, EnderecosSerializer, UnidadesFederativasSerializer, MunicipiosSerializer, ListaEnderecoClienteSerializer
-from rest_framework import viewsets, generics
+from rest_framework import viewsets, generics, response
 from cadastro.models import Cliente, Endereco, UnidadeFederativa, Municipio, Cartao
 from rest_framework.authentication import BasicAuthentication
 from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenViewBase,
+)
+from rest_framework_simplejwt.serializers import(
+    TokenObtainPairSerializer,
+)
+
 
 
 # Create your views here.
@@ -14,8 +24,16 @@ class ClientesViewSet(viewsets.ModelViewSet):
     queryset = Cliente.objects.all()
     serializer_class = ClientesSerializer
     permission_classes = [AllowAny]
-        
-        
+
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    default_error_messages = {
+        'no_active_account': '💀⚰️',
+    
+    }
+
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
+    
 
 class EnderecosViewSet(viewsets.ModelViewSet):
     """ Exibindo todos os Enderecos"""
--- setup/urls.py
+++ setup/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
-from cadastro.views import ClientesViewSet, EnderecosViewSet, ListaEnderecoClienteViewSet
+from cadastro.views import ClientesViewSet, EnderecosViewSet, ListaEnderecoClienteViewSet, MyTokenObtainPairView
 from rest_framework import routers, permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
@@ -30,7 +30,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include(router.urls) ),
     path('api-auth/', include('rest_framework.urls')),
-    path('api/token', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh', TokenRefreshView.as_view(), name='token_refresh'),
     path('cliente/<int:pk>/enderecos/', ListaEnderecoClienteViewSet.as_view(), name='lista-endereco-cliente'),
     path('swagger/', schema.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
