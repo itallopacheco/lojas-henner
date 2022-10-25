0a118d1aed9bb7ac2780ded27df1b5b35f5a0a71
Itallo Pacheco <oicapivara@academico.ufs.brm>
2022-10-24 23:14:30 -0300

    fix: cors error

--- setup/settings.py
+++ setup/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'rest_framework_swagger',
     'rest_framework',
     'cpf_field',
+    'corsheaders',
 
     'cadastro',
 ]
@@ -62,11 +63,19 @@ REST_FRAMEWORK = {
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:5173',
+    'http://localhost:3000',
 ]
 
 ROOT_URLCONF = 'setup.urls'
